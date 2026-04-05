3828036e8e96d808327293d8a9f9617aef82658a
danilkova_anastasiya <danilkova905@gmail.com>
2026-04-05 11:47:59 +0300

    add new feature with change of color in figure

--- Paint.App/MainWindow.xaml.cs
+++ Paint.App/MainWindow.xaml.cs
@@ -17,10 +17,9 @@ namespace Paint.App
         private bool _isDrawingActive = false;
         private int? _currentSideCount = 5;
         
-
-      
         private IPlugin _selectedPlugin;
-
+      
+        private bool _isSelectedMode = false;
 
         public MainWindow()
         {
@@ -35,6 +34,10 @@ namespace Paint.App
             LoadPlugins();
         }
 
+
+
+
+        //
         private void LoadPlugins()
         {
             string pluginsDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Plugins");
@@ -68,25 +71,6 @@ namespace Paint.App
         }
 
 
-        private int? CountSideOfPolygon()
-        {
-            while (true)
-            {
-                string input = Microsoft.VisualBasic.Interaction.InputBox("Введите количество сторон многоугольника(3-8)", "Настройка фигуры", "5");
-
-                if (string.IsNullOrEmpty(input)) return null;
-
-
-                if (int.TryParse(input, out int result) && result >= 3 && result <= 8)
-                {
-                    return result;
-                }
-                
-                MessageBox.Show("Неверный ввод!!!Введите корректное число сторон(от 3 до 8)");
-
-            }
-            
-        }
 
 
         private void AddPluginButton(IPlugin plugin)
@@ -101,7 +85,8 @@ namespace Paint.App
             btn.Click += (s, e) =>
             {
                 _selectedPlugin = plugin;
-               
+                _isSelectedMode = false;
+                _selectedShape = null;
 
                 if(plugin.Name == "Многоугольник")
                 {
@@ -112,12 +97,36 @@ namespace Paint.App
             PluginsPanel.Children.Add(btn);
         }
 
+        //
+
         private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton != MouseButton.Left) return;
-            if (_selectedPlugin == null) return;
-
             Point mousePos = e.GetPosition(MainCanvas);
+
+            
+
+            if (_isSelectedMode)
+            {
+                _selectedShape = null;
+
+                for (int i = _shapes.Count - 1; i >= 0; i--)
+                {
+                    if(IsPointInShape(mousePos, _shapes[i]))
+                    {
+                        _selectedShape =  _shapes[i];
+                        ThicknessSlider.Value = _selectedShape.StrokeThickness;
+
+                        break;
+                    }
+                }
+                Redraw();
+                return;
+            }
+
+
+
+            if (_selectedPlugin == null) return;
             bool isPolyline = _selectedPlugin.Name == "Ломаная";
 
             if (isPolyline)
@@ -187,6 +196,8 @@ namespace Paint.App
         }
 
 
+
+
         private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (_isDrawingActive && _currentShape != null)
@@ -209,6 +220,7 @@ namespace Paint.App
         }
 
 
+
         private void FinishDrawing(MouseButtonEventArgs e)
         {
             if (e.ChangedButton != MouseButton.Left) return;
@@ -226,6 +238,8 @@ namespace Paint.App
                 Redraw();
             }
         }
+
+
 
 
         private void MainCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
@@ -250,8 +264,6 @@ namespace Paint.App
                 Redraw();
             }
         }
-
-
      
         private void Redraw()
         {
@@ -259,6 +271,11 @@ namespace Paint.App
             foreach (var shape in _shapes)
             {
                 shape.Draw(MainCanvas);
+                
+                if(shape == _selectedShape)
+                {
+                    DrawSelectionFrame(shape);
+                }
             }
 
             if (_currentShape != null)
@@ -267,6 +284,139 @@ namespace Paint.App
             }
         }
 
-    
+
+
+        //
+
+        private int? CountSideOfPolygon()
+        {
+            while (true)
+            {
+                string input = Microsoft.VisualBasic.Interaction.InputBox("Введите количество сторон многоугольника(3-8)", "Настройка фигуры", "5");
+
+                if (string.IsNullOrEmpty(input)) return null;
+
+
+                if (int.TryParse(input, out int result) && result >= 3 && result <= 8)
+                {
+                    return result;
+                }
+
+                MessageBox.Show("Неверный ввод!!!Введите корректное число сторон(от 3 до 8)");
+
+            }
+        }
+
+
+        //Add selection mode
+        private void SelectMode_Click(object sender, RoutedEventArgs e)
+        {
+            _isSelectedMode = true;
+            _selectedPlugin = null;
+            _selectedShape = null;
+            Redraw();
+        }
+
+        private void StrokeColorPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_selectedShape != null && StrokeColorPicker.SelectedItem is ComboBoxItem item)
+            {
+                _selectedShape.StrokeColor = (Brush)item.Tag;
+                Redraw();
+            }
+        }
+
+        private void FillColorPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(_selectedShape != null && FillColorPicker.SelectedItem is ComboBoxItem item )
+            {
+                _selectedShape.FillColor = (Brush)item.Tag;
+                Redraw() ;
+            }
+        }
+
+
+        private bool IsPointInShape(Point p, IShape shape)
+        {
+            if (shape.Points.Count < 2) return false;
+
+            double minX, maxX, minY, maxY;
+
+            // Логика для Многоугольника (Центр + Радиус)
+            if (shape.GetType().Name.Contains("Polygon") && shape.Points.Count == 2)
+            {
+                Point center = shape.Points[0];
+                Point radiusPoint = shape.Points[1];
+                double radius = Math.Sqrt(Math.Pow(radiusPoint.X - center.X, 2) + Math.Pow(radiusPoint.Y - center.Y, 2));
+
+                minX = center.X - radius;
+                maxX = center.X + radius;
+                minY = center.Y - radius;
+                maxY = center.Y + radius;
+            }
+            else // Для обычных фигур
+            {
+                minX = shape.Points.Min(pt => pt.X);
+                maxX = shape.Points.Max(pt => pt.X);
+                minY = shape.Points.Min(pt => pt.Y);
+                maxY = shape.Points.Max(pt => pt.Y);
+            }
+
+
+            return p.X >= minX - 5 && p.X <= maxX + 5 && p.Y >= minY - 5 && p.Y <= maxY + 5;
+        }
+
+
+        private void DrawSelectionFrame(IShape shape)
+        {
+            double minX, maxX, minY, maxY;
+
+           
+            if (shape.GetType().Name.Contains("Polygon") && shape.Points.Count == 2)
+            {
+                Point center = shape.Points[0];
+                Point radiusPoint = shape.Points[1];
+
+               
+                double radius = Math.Sqrt(Math.Pow(radiusPoint.X - center.X, 2) + Math.Pow(radiusPoint.Y - center.Y, 2));
+
+                minX = center.X - radius;
+                maxX = center.X + radius;
+                minY = center.Y - radius;
+                maxY = center.Y + radius;
+            }
+            else 
+            {
+                minX = shape.Points.Min(p => p.X);
+                maxX = shape.Points.Max(p => p.X);
+                minY = shape.Points.Min(p => p.Y);
+                maxY = shape.Points.Max(p => p.Y);
+            }
+
+
+            Rectangle rect = new Rectangle
+            {
+                Width = maxX - minX + 10,
+                Height = maxY - minY + 10,
+                Stroke = Brushes.DeepSkyBlue,
+                StrokeThickness = 1,
+                StrokeDashArray = new DoubleCollection() { 4, 2 }
+            };
+            Canvas.SetLeft(rect, minX - 5);
+            Canvas.SetTop(rect, minY - 5);
+            MainCanvas.Children.Add(rect);
+        }
+
+        private void ThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_selectedShape != null )
+            {
+                _selectedShape.StrokeThickness = ThicknessSlider.Value;
+                Redraw();
+            }
+        }
+
+
+        //
     }
 }
